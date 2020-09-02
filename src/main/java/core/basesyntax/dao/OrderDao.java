aa579c454dd521c39eac19302e3bfc6dd659fd67
Laszlo Petechuk <frostpv@gmail.com>
2020-09-02 16:30:12 +0300

    Added Dao for all models

---
+++ src/main/java/core/basesyntax/dao/OrderDao.java
@@ -0,0 +1,17 @@
+package core.basesyntax.dao;
+
+import core.basesyntax.models.Order;
+import java.util.List;
+import java.util.Optional;
+
+public interface OrderDao {
+    Order create(Order order);
+
+    Optional<Order> get(Long id);
+
+    List<Order> getAll();
+
+    Order update(Order order);
+
+    boolean delete(Long id);
+}
