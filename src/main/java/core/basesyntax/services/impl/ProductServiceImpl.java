b3945e737e309e8232df5cf3269e17ed46c762b0
Laszlo Petechuk <frostpv@gmail.com>
2020-09-02 20:24:34 +0300

    Added ProductServiceImpl & ProductServiceInterface

---
+++ src/main/java/core/basesyntax/services/impl/ProductServiceImpl.java
@@ -0,0 +1,35 @@
+package core.basesyntax.services.impl;
+
+import core.basesyntax.lib.Services;
+import core.basesyntax.models.Product;
+import core.basesyntax.services.ProductServiceInterface;
+
+import java.util.List;
+
+@Services
+public class ProductServiceImpl implements ProductServiceInterface {
+    @Override
+    public Product create(Product product) {
+        return null;
+    }
+
+    @Override
+    public Product get(Long id) {
+        return null;
+    }
+
+    @Override
+    public List<Product> getAll() {
+        return null;
+    }
+
+    @Override
+    public Product update(Product product) {
+        return null;
+    }
+
+    @Override
+    public boolean delete(Long id) {
+        return false;
+    }
+}
