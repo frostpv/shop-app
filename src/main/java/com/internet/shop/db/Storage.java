2cd7c7345715df3b9d941188f4e76bb995f059ae
Laszlo Petechuk <frostpv@gmail.com>
2020-09-03 16:26:05 +0300

    Deleted unusual dao

--- src/main/java/com/internet/shop/db/Storage.java
+++ src/main/java/com/internet/shop/db/Storage.java
@@ -1,21 +1,12 @@
 package com.internet.shop.db;
 
-import com.internet.shop.models.Order;
 import com.internet.shop.models.Product;
-import com.internet.shop.models.ShoppingCart;
-import com.internet.shop.models.User;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Storage {
     public static List<Product> products = new ArrayList<>();
-    public static List<Order> orders = new ArrayList<>();
-    public static List<ShoppingCart> shoppingCarts = new ArrayList<>();
-    public static List<User> users = new ArrayList<>();
     private static long productId = 0;
-    private static long orderId = 0;
-    private static long shoppingCardId = 0;
-    private static long userId = 0;
 
     public static Product addProduct(Product product) {
         if (isNotNull(product)) {
@@ -23,27 +14,6 @@ public class Storage {
             products.add(product);
         }
         return product;
-    }
-
-    public static void addOrder(Order order) {
-        if (isNotNull(order)) {
-            order.setId(++orderId);
-            orders.add(order);
-        }
-    }
-
-    public static void addCart(ShoppingCart shoppingCart) {
-        if (isNotNull(shoppingCart)) {
-            shoppingCart.setId(++shoppingCardId);
-            shoppingCarts.add(shoppingCart);
-        }
-    }
-
-    public static void addUser(User user) {
-        if (isNotNull(user)) {
-            user.setId(++userId);
-            users.add(user);
-        }
     }
 
     private static boolean isNotNull(Object object) {
