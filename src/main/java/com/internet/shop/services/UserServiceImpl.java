df345594adb8a6c95d06d42487df84d26d1d119e
Laszlo Petechuk <frostpv@gmail.com>
2020-09-07 15:22:19 +0300

    Added UserServiceImpl

---
+++ src/main/java/com/internet/shop/services/UserServiceImpl.java
@@ -0,0 +1,44 @@
+package com.internet.shop.services;
+
+import com.internet.shop.dao.UserDao;
+import com.internet.shop.lib.Inject;
+import com.internet.shop.lib.Service;
+import com.internet.shop.models.User;
+import java.util.List;
+
+@Service
+public class UserServiceImpl implements UserServiceInterface {
+
+    @Inject
+    private UserDao userDao;
+
+    @Override
+    public User create(User user) {
+        return userDao.create(user);
+    }
+
+    @Override
+    public User get(Long id) {
+        return userDao
+                .get(id)
+                .orElseThrow();
+    }
+
+    @Override
+    public List<User> getAll() {
+        return userDao.getAll();
+    }
+
+    @Override
+    public User update(User user) {
+        if (user.getId().equals(get(user.getId()).getId())) {
+            return userDao.update(user);
+        }
+        throw new RuntimeException("User is not found");
+    }
+
+    @Override
+    public boolean delete(Long id) {
+        return userDao.delete(id);
+    }
+}
--- src/main/java/com/internet/shop/services/UserServiceInterface.java
+++ src/main/java/com/internet/shop/services/UserServiceInterface.java
@@ -4,6 +4,8 @@ import com.internet.shop.models.User;
 import java.util.List;
 
 public interface UserServiceInterface {
+
+
     User create(User user);
 
     User get(Long id);
