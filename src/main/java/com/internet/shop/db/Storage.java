8c32cada175c55fd5c7fa501d7993a69cdbe4bad
Laszlo Petechuk <frostpv@gmail.com>
2020-09-07 12:53:52 +0300

    Added lists & methods add in Storage

--- src/main/java/com/internet/shop/db/Storage.java
+++ src/main/java/com/internet/shop/db/Storage.java
@@ -1,12 +1,21 @@
 package com.internet.shop.db;
 
+import com.internet.shop.models.Order;
 import com.internet.shop.models.Product;
+import com.internet.shop.models.ShoppingCart;
+import com.internet.shop.models.User;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Storage {
     public static List<Product> products = new ArrayList<>();
+    public static List<User> users = new ArrayList<>();
+    public static List<Order> orders = new ArrayList<>();
+    public static List<ShoppingCart> shoppingCarts = new ArrayList<>();
     private static long productId = 0;
+    private static long userId = 0;
+    private static long orderId = 0;
+    private static long shopingCartId = 0;
 
     public static Product addProduct(Product product) {
         if (isNotNull(product)) {
@@ -14,6 +23,30 @@ public class Storage {
             products.add(product);
         }
         return product;
+    }
+
+    public static User addUser(User user) {
+        if (isNotNull(user)) {
+            user.setId(++userId);
+            users.add(user);
+        }
+        return user;
+    }
+
+    public static Order addOrder(Order order) {
+        if (isNotNull(order)) {
+            order.setId(++orderId);
+            orders.add(order);
+        }
+        return order;
+    }
+
+    public static ShoppingCart addShopingCart(ShoppingCart shoppingCart) {
+        if (isNotNull(shoppingCart)) {
+            shoppingCart.setId(++shopingCartId);
+            shoppingCarts.add(shoppingCart);
+        }
+        return shoppingCart;
     }
 
     private static boolean isNotNull(Object object) {
