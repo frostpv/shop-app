3bfc3d496cba8d9608fa974ecebc37f382f9e196
Laszlo Petechuk <frostpv@gmail.com>
2020-09-07 14:54:31 +0300

    Added services interfaces

---
+++ src/main/java/com/internet/shop/services/UserServiceInterface.java
@@ -0,0 +1,16 @@
+package com.internet.shop.services;
+
+import com.internet.shop.models.User;
+import java.util.List;
+
+public interface UserServiceInterface {
+    User create(User user);
+
+    User get(Long id);
+
+    List<User> getAll();
+
+    User update(User user);
+
+    boolean delete(Long id);
+}
