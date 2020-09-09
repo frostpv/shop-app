7f92480a54c0374bf5dbb899c39f49e985efc437
Laszlo Petechuk <frostpv@gmail.com>
2020-09-09 21:06:45 +0300

    added ProductAddController.java && add.jsp

---
+++ src/main/java/com/internet/shop/controllers/ProductAddController.java
@@ -0,0 +1,35 @@
+package com.internet.shop.controllers;
+
+import com.internet.shop.lib.Injector;
+import com.internet.shop.models.Product;
+import com.internet.shop.services.ProductService;
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class ProductAddController extends HttpServlet {
+    private static final Injector injector = Injector.getInstance("com.internet.shop");
+    private ProductService productService = (ProductService) injector.getInstance(ProductService.class);
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+
+        req.getRequestDispatcher("/WEB-INF/views/product/add.jsp").forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        String name = req.getParameter("name");
+        String price = req.getParameter("price");
+        if (name.length() >= 1 && price.length() >= 1) {
+            Product product = new Product(name, Long.parseLong(price));
+            productService.create(product);
+            resp.sendRedirect(req.getContextPath() + "/products");
+        } else {
+            req.getRequestDispatcher("/WEB-INF/views/product/add.jsp").forward(req, resp);
+        }
+    }
+}
---
+++ src/main/webapp/WEB-INF/views/product/add.jsp
@@ -0,0 +1,24 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Add product in Shop</title>
+</head>
+<body>
+<h1>Add product</h1>
+<p>
+    <a href="${pageContext.request.contextPath}/">HomePage</a> |
+    <a href="${pageContext.request.contextPath}/products">Products</a> |
+    <a href="${pageContext.request.contextPath}/products/add">Add product</a> |
+    <a href="${pageContext.request.contextPath}/users">Users</a> |
+    <a href="${pageContext.request.contextPath}/users/registration">Regisration</a> |
+    <a href="${pageContext.request.contextPath}/cart">Cart</a>
+</p>
+<p>
+    <form method="post" action="${pageContext.request.contextPath}/products/add">
+        Please provide product name: <input type="text" name="name"></br>
+        Please provide product price: <input type="text" name="price"></br>
+        <input type="submit" value="Register"></br>
+    </form>
+</p>
+</body>
+</html>
