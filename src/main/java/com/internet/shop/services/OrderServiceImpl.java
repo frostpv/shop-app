71a3254d5015f120b9dfd2aa9952aa45317097fc
Laszlo Petechuk <frostpv@gmail.com>
2020-09-09 08:12:45 +0300

    Added realisation for order methods

--- src/main/java/com/internet/shop/services/OrderServiceImpl.java
+++ src/main/java/com/internet/shop/services/OrderServiceImpl.java
@@ -32,7 +32,7 @@ public class OrderServiceImpl implements OrderService {
 
     @Override
     public Order create(Order item) {
-        return null;
+        return orderDao.create(item);
     }
 
     @Override
@@ -47,7 +47,7 @@ public class OrderServiceImpl implements OrderService {
 
     @Override
     public Order update(Order item) {
-        return null;
+        return orderDao.update(item);
     }
 
     @Override
