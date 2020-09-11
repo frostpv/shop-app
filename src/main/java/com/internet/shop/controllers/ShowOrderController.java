cbd9ace1b1414d35a8c17c20370429a65db10f5d
Laszlo Petechuk <frostpv@gmail.com>
2020-09-11 15:47:42 +0300

    added ShowOrderController

---
+++ src/main/java/com/internet/shop/controllers/ShowOrderController.java
@@ -0,0 +1,31 @@
+package com.internet.shop.controllers;
+
+import com.internet.shop.lib.Injector;
+import com.internet.shop.models.Product;
+import com.internet.shop.services.OrderService;
+import java.io.IOException;
+import java.util.List;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class ShowOrderController extends HttpServlet {
+    private static final Injector injector =
+            Injector.getInstance("com.internet.shop");
+    private OrderService orderService =
+            (OrderService) injector.getInstance(OrderService.class);
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        String id = req.getParameter("id");
+        List<Product> products = orderService.get(Long.parseLong(id)).getProducts();
+        double sum = products.stream()
+                .mapToDouble(Product::getPrice)
+                .sum();
+        req.setAttribute("sum", sum);
+        req.setAttribute("products", products);
+        req.getRequestDispatcher("/WEB-INF/views/order/order.jsp").forward(req, resp);
+    }
+}
--- src/main/webapp/WEB-INF/views/order/order.jsp
+++ src/main/webapp/WEB-INF/views/order/order.jsp
@@ -1,3 +1,4 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
 <head>
@@ -15,5 +16,27 @@
     <a href="${pageContext.request.contextPath}/orders">Orders</a>
     <a href="${pageContext.request.contextPath}/orders/list">Orders list</a>
 </p>
+<table border="1">
+    <tr>
+        <td><strong>Product ID</strong></td>
+        <td><strong>Name</strong></td>
+        <td><strong>Price</strong></td>
+        <c:forEach var="product" items="${products}">
+    <tr>
+        <td><c:out value="${product.id}"/></td>
+        <td><c:out value="${product.name}"/></td>
+        <td><c:out value="${product.price}"/></td>
+        <td><form action="${pageContext.request.contextPath}/order" method="post">
+            <input hidden name = "id" value="${product.id}">
+            <input type="submit" value="show order">
+        </form></td>
+    </tr>
+    </c:forEach>
+    <tr>
+        <td></td>
+        <td></td>
+        <td>${sum} uah</td>
+    </tr>
+</table>
 </body>
 </html>
