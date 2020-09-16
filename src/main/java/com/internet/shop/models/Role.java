c29ed2bd2f04c88c7373bd9b1d9a99eaf92955a6
Laszlo Petechuk <frostpv@gmail.com>
2020-09-16 12:26:42 +0300

    added model Role

---
+++ src/main/java/com/internet/shop/models/Role.java
@@ -0,0 +1,34 @@
+package com.internet.shop.models;
+
+public class Role {
+    private Long id;
+    private RoleName roleName;
+
+    public Role(RoleName roleName) {
+        this.roleName = roleName;
+    }
+
+    public static Role of(String roleName) {
+        return new Role(RoleName.valueOf(roleName));
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public RoleName getRoleName() {
+        return roleName;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setRoleName(RoleName roleName) {
+        this.roleName = roleName;
+    }
+
+    public enum RoleName {
+        USER, ADMIN;
+    }
+}
