8156adc8449198db4a2c9476ccc37d5177a05b6a
Laszlo Petechuk <frostpv@gmail.com>
2020-09-21 13:06:34 +0300

    fixed main

--- src/main/java/com/internet/shop/WebShopApp.java
+++ src/main/java/com/internet/shop/WebShopApp.java
@@ -7,10 +7,14 @@ public class WebShopApp {
     public static void main(String[] args) {
         ProductDaoJdbcImpl daoJdbc = new ProductDaoJdbcImpl();
         Product product = new Product("BMW M5", 54555);
-        System.out.println(daoJdbc.create(product).toString());
-        System.out.println(daoJdbc.get(1L).toString());
-        daoJdbc.delete(1L);
+        Product product1 = daoJdbc.create(product);
         System.out.println(daoJdbc.getAll());
-
+        System.out.println(daoJdbc.get(product1.getId()).toString());
+        System.out.println(daoJdbc.getAll());
+        product1.setPrice(99999);
+        daoJdbc.update(product1);
+        System.out.println(daoJdbc.getAll());
+        daoJdbc.delete(product1.getId());
+        System.out.println(daoJdbc.getAll());
     }
 }
