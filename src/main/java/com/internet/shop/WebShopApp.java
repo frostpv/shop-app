e44dbfd4791ce2d91cd69434f83b1e9c82445c3a
Laszlo Petechuk <frostpv@gmail.com>
2020-09-21 15:01:02 +0300

    used injector

--- src/main/java/com/internet/shop/WebShopApp.java
+++ src/main/java/com/internet/shop/WebShopApp.java
@@ -1,20 +1,24 @@
 package com.internet.shop;
 
-import com.internet.shop.dao.ProductDaoJdbcImpl;
+import com.internet.shop.lib.Injector;
 import com.internet.shop.models.Product;
+import com.internet.shop.services.ProductService;
 
 public class WebShopApp {
     public static void main(String[] args) {
-        ProductDaoJdbcImpl daoJdbc = new ProductDaoJdbcImpl();
+        ProductService productService =
+                (ProductService) Injector.getInstance("com.internet.shop")
+                        .getInstance(ProductService.class);
+
         Product product = new Product("BMW M5", 54555);
-        Product product1 = daoJdbc.create(product);
-        System.out.println(daoJdbc.getAll());
-        System.out.println(daoJdbc.get(product1.getId()).toString());
-        System.out.println(daoJdbc.getAll());
+        Product product1 = productService.create(product);
+        System.out.println(productService.getAll());
+        System.out.println(productService.get(product1.getId()).toString());
+        System.out.println(productService.getAll());
         product1.setPrice(99999);
-        daoJdbc.update(product1);
-        System.out.println(daoJdbc.getAll());
-        daoJdbc.delete(product1.getId());
-        System.out.println(daoJdbc.getAll());
+        productService.update(product1);
+        System.out.println(productService.getAll());
+        productService.delete(product1.getId());
+        System.out.println(productService.getAll());
     }
 }
