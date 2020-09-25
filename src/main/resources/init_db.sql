de82566ebcdd658c88f7530554cca4464dae9ad6
Laszlo Petechuk <frostpv@gmail.com>
2020-09-25 17:57:22 +0300

    add db in to resources

--- src/main/resources/init_db.sql
+++ src/main/resources/init_db.sql
@@ -23,14 +23,24 @@ DROP TABLE IF EXISTS `orders`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `orders` (
-  `order_id` bigint NOT NULL,
+  `order_id` bigint NOT NULL AUTO_INCREMENT,
   `user_id` bigint DEFAULT NULL,
   `deleted` tinyint DEFAULT '0',
   PRIMARY KEY (`order_id`),
   KEY `fk_users_id_idx` (`user_id`),
   CONSTRAINT `fk_users_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=26 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `orders`
+--
+
+LOCK TABLES `orders` WRITE;
+/*!40000 ALTER TABLE `orders` DISABLE KEYS */;
+INSERT INTO `orders` VALUES (1,2,1),(2,2,1),(3,2,1),(4,2,1),(5,4,1),(6,4,1),(7,4,1),(8,4,1),(9,4,1),(10,4,1),(11,2,1),(12,4,1),(13,4,1),(14,4,1),(15,5,1),(16,4,1),(17,4,1),(18,4,1),(19,4,1),(20,4,1),(21,6,1),(22,1,1),(23,6,1),(24,6,1),(25,6,0);
+/*!40000 ALTER TABLE `orders` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `orders_products`
@@ -42,12 +52,22 @@ DROP TABLE IF EXISTS `orders_products`;
 CREATE TABLE `orders_products` (
   `order_id` bigint DEFAULT NULL,
   `product_id` bigint DEFAULT NULL,
-  KEY `fk_orders_id_idx` (`order_id`),
   KEY `fk_productc_id_idx` (`product_id`),
+  KEY `fk_orders_id_idx` (`order_id`),
   CONSTRAINT `fk_orders_id` FOREIGN KEY (`order_id`) REFERENCES `orders` (`order_id`),
   CONSTRAINT `fk_productc_id` FOREIGN KEY (`product_id`) REFERENCES `products` (`product_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `orders_products`
+--
+
+LOCK TABLES `orders_products` WRITE;
+/*!40000 ALTER TABLE `orders_products` DISABLE KEYS */;
+INSERT INTO `orders_products` VALUES (3,1),(3,1),(3,1),(3,1),(4,1),(5,1),(5,1),(6,1),(6,1),(7,1),(7,2),(8,1),(9,1),(10,1),(11,1),(12,1),(13,1),(14,2),(15,2),(16,1),(17,1),(19,1),(20,1),(21,1),(22,1),(23,1),(24,1),(25,1);
+/*!40000 ALTER TABLE `orders_products` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `products`
@@ -62,8 +82,18 @@ CREATE TABLE `products` (
   `price` double NOT NULL,
   `deleted` tinyint DEFAULT '0',
   PRIMARY KEY (`product_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `products`
+--
+
+LOCK TABLES `products` WRITE;
+/*!40000 ALTER TABLE `products` DISABLE KEYS */;
+INSERT INTO `products` VALUES (1,'Audi Q5',45686,0),(2,'Audi Q5',51515,1);
+/*!40000 ALTER TABLE `products` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `roles`
@@ -76,8 +106,18 @@ CREATE TABLE `roles` (
   `role_id` bigint NOT NULL AUTO_INCREMENT,
   `role_name` varchar(256) NOT NULL,
   PRIMARY KEY (`role_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `roles`
+--
+
+LOCK TABLES `roles` WRITE;
+/*!40000 ALTER TABLE `roles` DISABLE KEYS */;
+INSERT INTO `roles` VALUES (1,'ADMIN'),(2,'USER');
+/*!40000 ALTER TABLE `roles` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `shoping_cart`
@@ -93,8 +133,18 @@ CREATE TABLE `shoping_cart` (
   PRIMARY KEY (`id_shoping_cart`),
   KEY `fk_userss_ids_idx` (`id_user`),
   CONSTRAINT `fk_userss_ids` FOREIGN KEY (`id_user`) REFERENCES `users` (`user_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `shoping_cart`
+--
+
+LOCK TABLES `shoping_cart` WRITE;
+/*!40000 ALTER TABLE `shoping_cart` DISABLE KEYS */;
+INSERT INTO `shoping_cart` VALUES (1,1,1),(2,3,1),(3,4,0),(4,5,0),(5,6,0),(6,7,1);
+/*!40000 ALTER TABLE `shoping_cart` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `shoping_cart_products`
@@ -114,6 +164,16 @@ CREATE TABLE `shoping_cart_products` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
+-- Dumping data for table `shoping_cart_products`
+--
+
+LOCK TABLES `shoping_cart_products` WRITE;
+/*!40000 ALTER TABLE `shoping_cart_products` DISABLE KEYS */;
+INSERT INTO `shoping_cart_products` VALUES (4,2),(2,1),(3,1),(1,1),(1,1);
+/*!40000 ALTER TABLE `shoping_cart_products` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `user_roles`
 --
 
@@ -131,6 +191,16 @@ CREATE TABLE `user_roles` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
+-- Dumping data for table `user_roles`
+--
+
+LOCK TABLES `user_roles` WRITE;
+/*!40000 ALTER TABLE `user_roles` DISABLE KEYS */;
+INSERT INTO `user_roles` VALUES (1,2),(2,1),(3,2),(4,2),(2,1),(5,2),(6,2),(7,2);
+/*!40000 ALTER TABLE `user_roles` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `users`
 --
 
@@ -144,8 +214,18 @@ CREATE TABLE `users` (
   `user_pass` varchar(226) DEFAULT NULL,
   `deleted` tinyint DEFAULT '0',
   PRIMARY KEY (`user_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'Igor','Igor','123',1),(2,'Admin','Admin','123',0),(3,'Laci','Laci','123',1),(4,'Max','Max','123',0),(5,'Dima','Dima','123',0),(6,'Mika','Mika','123',0),(7,'Igor','Igor','123',1);
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -156,4 +236,4 @@ CREATE TABLE `users` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-09-24 19:20:17
+-- Dump completed on 2020-09-25 17:55:41
