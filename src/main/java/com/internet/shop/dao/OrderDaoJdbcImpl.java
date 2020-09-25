1b7482e6f7e89630a9cceb7949b480e8eb757164
Laszlo Petechuk <frostpv@gmail.com>
2020-09-25 17:42:20 +0300

    fixed admin show orders part

--- src/main/java/com/internet/shop/dao/OrderDaoJdbcImpl.java
+++ src/main/java/com/internet/shop/dao/OrderDaoJdbcImpl.java
@@ -87,7 +87,7 @@ public class OrderDaoJdbcImpl implements OrderDao {
             preparedStatement.setLong(1, id);
             ResultSet resultSet = preparedStatement.executeQuery();
             if (resultSet.next()) {
-                order.setIdUser(resultSet.getLong("id_user"));
+                order.setIdUser(resultSet.getLong("user_id"));
                 order.setId(id);
             } else {
                 return Optional.empty();
@@ -131,15 +131,18 @@ public class OrderDaoJdbcImpl implements OrderDao {
             PreparedStatement preparedStatement = connection.prepareStatement(query);
             ResultSet resultSet = preparedStatement.executeQuery();
             while (resultSet.next()) {
-                Order order = new Order(resultSet.getLong("order_id"));
+                Order order = new Order();
+                order.setId(resultSet.getLong("order_id"));
                 order.setIdUser(resultSet.getLong("user_id"));
-                //order.setProducts(getOrderProducts(order.getId()));
                 orders.add(order);
             }
         } catch (SQLException e) {
             throw new DataBaseProcessingException("Order list was not created", e);
         }
-        orders.forEach(this::getOrderProducts);
+
+        for (Order order : orders) {
+            getOrderProducts(order);
+        }
         return orders;
     }
 
