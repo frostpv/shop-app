1df77e1ae1a2560ed81b319a1ba05b1cd12cd721
Laszlo Petechuk <frostpv@gmail.com>
2020-09-25 17:21:11 +0300

    fixed Product.java

--- src/main/java/com/internet/shop/models/Product.java
+++ src/main/java/com/internet/shop/models/Product.java
@@ -41,4 +41,19 @@ public class Product {
                 + ", name='" + name + '\''
                 + ", price=" + price + '}';
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Product product = (Product) o;
+
+        return id != null ? id.equals(product.id) : product.id == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
 }
