d4e6907e06e78e001e64f5e5b2802dfc27fcac72
Laszlo Petechuk <frostpv@gmail.com>
2020-09-27 13:17:59 +0300

    deleted comments in file

--- src/main/resources/init_db.sql
+++ src/main/resources/init_db.sql
@@ -1,27 +1,4 @@
--- MySQL dump 10.13  Distrib 8.0.21, for Win64 (x86_64)
---
--- Host: localhost    Database: shop
--- ------------------------------------------------------
--- Server version	8.0.21
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
---
--- Table structure for table `orders`
---
-
 DROP TABLE IF EXISTS `orders`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `orders` (
   `order_id` bigint NOT NULL AUTO_INCREMENT,
   `user_id` bigint DEFAULT NULL,
@@ -30,25 +7,10 @@ CREATE TABLE `orders` (
   KEY `fk_users_id_idx` (`user_id`),
   CONSTRAINT `fk_users_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=26 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `orders`
---
-
 LOCK TABLES `orders` WRITE;
-/*!40000 ALTER TABLE `orders` DISABLE KEYS */;
 INSERT INTO `orders` VALUES (1,2,1),(2,2,1),(3,2,1),(4,2,1),(5,4,1),(6,4,1),(7,4,1),(8,4,1),(9,4,1),(10,4,1),(11,2,1),(12,4,1),(13,4,1),(14,4,1),(15,5,1),(16,4,1),(17,4,1),(18,4,1),(19,4,1),(20,4,1),(21,6,1),(22,1,1),(23,6,1),(24,6,1),(25,6,0);
-/*!40000 ALTER TABLE `orders` ENABLE KEYS */;
 UNLOCK TABLES;
-
---
--- Table structure for table `orders_products`
---
-
 DROP TABLE IF EXISTS `orders_products`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `orders_products` (
   `order_id` bigint DEFAULT NULL,
   `product_id` bigint DEFAULT NULL,
@@ -57,25 +19,10 @@ CREATE TABLE `orders_products` (
   CONSTRAINT `fk_orders_id` FOREIGN KEY (`order_id`) REFERENCES `orders` (`order_id`),
   CONSTRAINT `fk_productc_id` FOREIGN KEY (`product_id`) REFERENCES `products` (`product_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `orders_products`
---
-
 LOCK TABLES `orders_products` WRITE;
-/*!40000 ALTER TABLE `orders_products` DISABLE KEYS */;
 INSERT INTO `orders_products` VALUES (3,1),(3,1),(3,1),(3,1),(4,1),(5,1),(5,1),(6,1),(6,1),(7,1),(7,2),(8,1),(9,1),(10,1),(11,1),(12,1),(13,1),(14,2),(15,2),(16,1),(17,1),(19,1),(20,1),(21,1),(22,1),(23,1),(24,1),(25,1);
-/*!40000 ALTER TABLE `orders_products` ENABLE KEYS */;
 UNLOCK TABLES;
-
---
--- Table structure for table `products`
---
-
 DROP TABLE IF EXISTS `products`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `products` (
   `product_id` bigint NOT NULL AUTO_INCREMENT,
   `name` varchar(256) NOT NULL,
@@ -83,49 +30,19 @@ CREATE TABLE `products` (
   `deleted` tinyint DEFAULT '0',
   PRIMARY KEY (`product_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `products`
---
-
 LOCK TABLES `products` WRITE;
-/*!40000 ALTER TABLE `products` DISABLE KEYS */;
 INSERT INTO `products` VALUES (1,'Audi Q5',45686,0),(2,'Audi Q5',51515,1);
-/*!40000 ALTER TABLE `products` ENABLE KEYS */;
 UNLOCK TABLES;
-
---
--- Table structure for table `roles`
---
-
 DROP TABLE IF EXISTS `roles`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `roles` (
   `role_id` bigint NOT NULL AUTO_INCREMENT,
   `role_name` varchar(256) NOT NULL,
   PRIMARY KEY (`role_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `roles`
---
-
 LOCK TABLES `roles` WRITE;
-/*!40000 ALTER TABLE `roles` DISABLE KEYS */;
 INSERT INTO `roles` VALUES (1,'ADMIN'),(2,'USER');
-/*!40000 ALTER TABLE `roles` ENABLE KEYS */;
 UNLOCK TABLES;
-
---
--- Table structure for table `shoping_cart`
---
-
 DROP TABLE IF EXISTS `shoping_cart`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `shoping_cart` (
   `id_shoping_cart` bigint NOT NULL AUTO_INCREMENT,
   `id_user` bigint DEFAULT NULL,
@@ -134,25 +51,10 @@ CREATE TABLE `shoping_cart` (
   KEY `fk_userss_ids_idx` (`id_user`),
   CONSTRAINT `fk_userss_ids` FOREIGN KEY (`id_user`) REFERENCES `users` (`user_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `shoping_cart`
---
-
 LOCK TABLES `shoping_cart` WRITE;
-/*!40000 ALTER TABLE `shoping_cart` DISABLE KEYS */;
 INSERT INTO `shoping_cart` VALUES (1,1,1),(2,3,1),(3,4,0),(4,5,0),(5,6,0),(6,7,1);
-/*!40000 ALTER TABLE `shoping_cart` ENABLE KEYS */;
 UNLOCK TABLES;
-
---
--- Table structure for table `shoping_cart_products`
---
-
 DROP TABLE IF EXISTS `shoping_cart_products`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `shoping_cart_products` (
   `id_cart` bigint DEFAULT NULL,
   `id_product` bigint DEFAULT NULL,
@@ -161,25 +63,10 @@ CREATE TABLE `shoping_cart_products` (
   CONSTRAINT `fk_cart_id` FOREIGN KEY (`id_cart`) REFERENCES `shoping_cart` (`id_shoping_cart`),
   CONSTRAINT `fk_product_id` FOREIGN KEY (`id_product`) REFERENCES `products` (`product_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `shoping_cart_products`
---
-
 LOCK TABLES `shoping_cart_products` WRITE;
-/*!40000 ALTER TABLE `shoping_cart_products` DISABLE KEYS */;
 INSERT INTO `shoping_cart_products` VALUES (4,2),(2,1),(3,1),(1,1),(1,1);
-/*!40000 ALTER TABLE `shoping_cart_products` ENABLE KEYS */;
 UNLOCK TABLES;
-
---
--- Table structure for table `user_roles`
---
-
 DROP TABLE IF EXISTS `user_roles`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `user_roles` (
   `user_id` bigint DEFAULT NULL,
   `role_id` bigint DEFAULT NULL,
@@ -188,25 +75,10 @@ CREATE TABLE `user_roles` (
   CONSTRAINT `fk_role_id` FOREIGN KEY (`role_id`) REFERENCES `roles` (`role_id`),
   CONSTRAINT `user_fk_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `user_roles`
---
-
 LOCK TABLES `user_roles` WRITE;
-/*!40000 ALTER TABLE `user_roles` DISABLE KEYS */;
 INSERT INTO `user_roles` VALUES (1,2),(2,1),(3,2),(4,2),(2,1),(5,2),(6,2),(7,2);
-/*!40000 ALTER TABLE `user_roles` ENABLE KEYS */;
 UNLOCK TABLES;
-
---
--- Table structure for table `users`
---
-
 DROP TABLE IF EXISTS `users`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `users` (
   `user_id` bigint NOT NULL AUTO_INCREMENT,
   `user_name` varchar(226) DEFAULT NULL,
@@ -215,25 +87,6 @@ CREATE TABLE `users` (
   `deleted` tinyint DEFAULT '0',
   PRIMARY KEY (`user_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `users`
---
-
 LOCK TABLES `users` WRITE;
-/*!40000 ALTER TABLE `users` DISABLE KEYS */;
 INSERT INTO `users` VALUES (1,'Igor','Igor','123',1),(2,'Admin','Admin','123',0),(3,'Laci','Laci','123',1),(4,'Max','Max','123',0),(5,'Dima','Dima','123',0),(6,'Mika','Mika','123',0),(7,'Igor','Igor','123',1);
-/*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2020-09-25 17:55:41
