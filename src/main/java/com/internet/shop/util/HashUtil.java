b32f2b08a1bc03b3c5943800df516b378390daf1
Laszlo Petechuk <frostpv@gmail.com>
2020-09-29 16:55:59 +0300

    added HashUtil methods

--- src/main/java/com/internet/shop/util/HashUtil.java
+++ src/main/java/com/internet/shop/util/HashUtil.java
@@ -1,4 +1,32 @@
 package com.internet.shop.util;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+
 public class HashUtil {
+    private static final String HASH_ALGORITHM = "SHA-512";
+
+    public static byte[] getSalt() {
+        SecureRandom secureRandom = new SecureRandom();
+        byte[] salt = new byte[16];
+        secureRandom.nextBytes(salt);
+        return salt;
+    }
+
+    public String hashPassword(String password, byte[] salt) {
+        StringBuilder hashedPassword = new StringBuilder();
+        try {
+            MessageDigest messageDigest = MessageDigest.getInstance(HASH_ALGORITHM);
+            messageDigest.update(salt);
+            byte[] digest = messageDigest.digest(password.getBytes());
+            for (byte b : digest) {
+                hashedPassword.append(String.format("%02x", b));
+            }
+
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        return hashedPassword.toString();
+    }
 }
