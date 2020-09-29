4bd8d890e46561693d853c63948d8cb67e468424
Laszlo Petechuk <frostpv@gmail.com>
2020-09-29 16:57:58 +0300

    added HashCreateException

---
+++ src/main/java/com/internet/shop/exceptions/HashCreateException.java
@@ -0,0 +1,7 @@
+package com.internet.shop.exceptions;
+
+public class HashCreateException extends RuntimeException {
+    public HashCreateException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
